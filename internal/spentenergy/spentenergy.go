1ca536cdd993296b9e05f6652bff80b690387fc9
Anna Polyakova <poteryannaya.anna@yandex.ru>
2025-04-01 18:23:03 +0300

    firstIt

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,9 @@
 package spentenergy
 
-import ...
+import (
+	"fmt"
+	"time"
+)
 
 // Основные константы, необходимые для расчетов.
 const (
@@ -28,8 +31,23 @@ const (
 // duration time.Duration — длительность тренировки.
 //
 // Создайте функцию ниже.
-...
-
+func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	// чек на отрицательный вес\рост менее
+	if weight < 0 || height < 0 {
+		return 0, fmt.Errorf("ошибка: вес или рост менее 0")
+	}
+	// чек на отрицательную продолжительность
+	if duration < 0 {
+		return 0, fmt.Errorf("ошибка: продолжительность не может быть отрицательной")
+	}
+	meanSpeed, err := MeanSpeed(steps, duration)
+	if err != nil {
+		return 0, fmt.Errorf("ошибка расчета средней скорости")
+	}
+	durationHours := duration.Hours()
+	spentCal := ((walkingCaloriesWeightMultiplier * weight) + (meanSpeed*meanSpeed/height)*walkingSpeedHeightMultiplier) * durationHours * minInH
+	return spentCal, nil
+}
 
 // Константы для расчета калорий, расходуемых при беге.
 const (
@@ -46,8 +64,20 @@ const (
 // duration time.Duration — длительность тренировки.
 //
 // Создайте функцию ниже.
-...
-
+func RunningSpentCalories(steps int, weight float64, duration time.Duration) (float64, error) {
+	if weight < 0 {
+		return 0, fmt.Errorf("ошибка: вес не может быть отрицательным")
+	}
+	if duration < 0 {
+		return 0, fmt.Errorf("ошибка: продолжительность не может быть отрицательной")
+	}
+	meanSpeed, err := MeanSpeed(steps, duration)
+	if err != nil {
+		return 0, fmt.Errorf("ошибка расчета средней скорости")
+	}
+	spentCal := ((runningCaloriesMeanSpeedMultiplier * meanSpeed) - runningCaloriesMeanSpeedShift) * weight
+	return spentCal, nil
+}
 
 // МeanSpeed возвращает значение средней скорости движения во время тренировки.
 //
@@ -55,10 +85,20 @@ const (
 //
 // steps int — количество совершенных действий(число шагов при ходьбе и беге).
 // duration time.Duration — длительность тренировки.
-// 
+//
 // Создайте функцию ниже.
-...
-
+func MeanSpeed(steps int, duration time.Duration) (float64, error) {
+	// чек на продолжительность < 0
+	if duration < 0 {
+		return 0, fmt.Errorf("ошибка: продолжительность не может быть отрицательной")
+	}
+	// получаем дистанцию из функции
+	distance := Distance(steps)
+	// конвертим время в часы
+	durationHours := duration.Hours()
+	meanSpeed := distance / durationHours
+	return meanSpeed, nil
+}
 
 // Distance возвращает дистанцию(в километрах), которую преодолел пользователь за время тренировки.
 //
@@ -66,8 +106,9 @@ const (
 // Параметры:
 //
 // steps int — количество совершенных действий (число шагов при ходьбе и беге).
-// 
+//
 // Создайте функцию ниже
-...
-
-
+func Distance(steps int) float64 {
+	distance := (float64(steps) * lenStep) / float64(mInKm)
+	return distance
+}
